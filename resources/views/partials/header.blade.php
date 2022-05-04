579ec8f01d2f9e7fed5f2986a7ac956e9b0b6f72
Massimo1204 <xiangmassimo@gmail.com>
2022-05-04 17:18:28 +0200

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,61 @@
+<header class="text-uppercase">
+    <div class="w-80">
+        <div>
+          <img src="{{asset("images/dc-logo.png")}}" alt="dc logo" />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a href="#">
+                characters
+              </a>
+            </li>
+            <li>
+              <a class="active" href="#">
+                comics
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                movies
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                tv
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                games
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                collectibles
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                videos
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                fans
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                news
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                shop
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $comics = config('comics');
+    return view('layouts.base', ["comics" => $comics]);
+})->name('home');
